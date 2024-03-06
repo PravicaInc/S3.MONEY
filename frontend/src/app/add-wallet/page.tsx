1af7a9582aaf1930f0e7b3a65afb643a2f962f18
Slavik Lutsenko <slavka.com.ua@gmail.com>
2024-03-06 12:36:07 +0200

    fix: remove old useWalletHook

--- frontend/src/app/add-wallet/page.tsx
+++ frontend/src/app/add-wallet/page.tsx
@@ -1,13 +1,11 @@
 import { Metadata } from 'next';
 
-import { AddWalletForm } from '@/Components/AddWalletForm';
-
 export const metadata: Metadata = {
   title: 'S3 - Add wallet',
 };
 
 export default function AddWalletPage() {
   return (
-    <AddWalletForm />
+    'AddWalletForm'
   );
 }
