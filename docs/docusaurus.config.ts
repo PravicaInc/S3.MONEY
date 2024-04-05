f2c7859ebe9445bb9869e7884358a11c38b2e41a
Imamah-Zafar <imamah.zafar@tintash.com>
2024-04-05 15:44:49 +0500

    Fix eslint issues

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,28 +1,28 @@
-import type * as Preset from "@docusaurus/preset-classic";
-import type { Config } from "@docusaurus/types";
-import { themes as prismThemes } from "prism-react-renderer";
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 const config: Config = {
-  title: "StableCoin ",
-  favicon: "/img/logo.svg",
-  url: "https://docs.s3.money.com",
-  baseUrl: "/",
+  title: 'StableCoin ',
+  favicon: '/img/logo.svg',
+  url: 'https://docs.s3.money.com',
+  baseUrl: '/',
 
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
+          sidebarPath: './sidebars.ts',
           // Please change this to your repo.
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -31,15 +31,15 @@ const config: Config = {
   themeConfig: {
     navbar: {
       logo: {
-        alt: "wrapper-async",
-        srcDark: "/img/full_logo_white.png",
-        src: "/img/full_logo.png",
+        alt: 'wrapper-async',
+        srcDark: '/img/full_logo_white.png',
+        src: '/img/full_logo.png',
       },
       items: [
         {
-          href: "https://github.com/PravicaInc/S3.MONEY",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/PravicaInc/S3.MONEY',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
