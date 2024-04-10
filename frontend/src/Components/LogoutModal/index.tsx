f03b8b1cac2c9fd5d2e2fe27f07167706c4eb872
Slavik Lutsenko <slavka.com.ua@gmail.com>
2024-04-10 21:25:06 +0300

    feat: Update logout

--- frontend/src/Components/LogoutModal/index.tsx
+++ frontend/src/Components/LogoutModal/index.tsx
@@ -1,4 +1,8 @@
 import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import AlertIcon from '@/../public/images/alert_icon.svg?jsx';
+import BackgroundModalDecorativeIcon from '@/../public/images/background_modal_decorative_left.svg?jsx';
 
 import { Button, BUTTON_VIEWS } from '@/Components/Form/Button';
 import { Modal, ModalProps } from '@/Components/Modal';
@@ -8,14 +12,25 @@ export interface LogoutModalProps extends ModalProps {
   inProcess?: boolean;
 }
 
-export const LogoutModal: FC<LogoutModalProps> = ({ onProceed, onClose, inProcess, ...props }) => (
+export const LogoutModal: FC<LogoutModalProps> = ({ onProceed, onClose, inProcess, className, ...props }) => (
   <Modal
     onClose={onClose}
-    className="p-6"
+    className={twMerge('relative p-6 w-[400px]', className)}
+    withCloseButton={false}
     {...props}
   >
-    <p className="text-primary font-semibold text-2xl text-center mt-4">
-      Confirm Logout?
+    <div className="absolute top-0 left-0 z-[-1]">
+      <BackgroundModalDecorativeIcon />
+
+      <div className="absolute top-6 left-6 bg-mistyRose w-12 h-12 flex items-center justify-center rounded-full">
+        <AlertIcon />
+      </div>
+    </div>
+    <p className="text-primary text-lg font-semibold mt-16">
+      Are you sure to Logout?
+    </p>
+    <p className="mt-1 text-secondary">
+      We can't wait to see you again! Please feel free to drop by anytime.
     </p>
     <div className="flex items-center justify-center gap-4 mt-8">
       <Button
@@ -30,7 +45,7 @@ export const LogoutModal: FC<LogoutModalProps> = ({ onProceed, onClose, inProces
         onClick={onProceed}
         isLoading={inProcess}
       >
-        Proceed
+        Logout
       </Button>
     </div>
   </Modal>
