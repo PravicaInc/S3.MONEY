31f4157115b7fd3c0bed6d56b72233f01eb9fc17
Imamah-Zafar <imamah.zafar@tintash.com>
2024-04-22 17:20:41 +0500

    add remaining test cases

--- backend/backend/backend-src/lib/test.ts
+++ backend/backend/backend-src/lib/test.ts
@@ -1,7 +1,7 @@
-import { validCancel, validCreate } from './checks';
+import { isValidTicker, validCancel, validCreate, validIconRequest, validPublish, validRelatedCreate, validRelatedDelete, validRelatedModify } from './checks';
 import * as packageOps from './db/packages';
 import * as IFace from './interfaces';
-import { PackageRoles, PackageStatus } from './interfaces';
+import { IRelationCreate, PackageRoles, PackageStatus } from './interfaces';
 
 jest.mock('./db/packages', () => ({
   getPackage: jest.fn(),
@@ -272,6 +272,7 @@ describe('validCancel function', () => {
     expect(result.data).toEqual(validInput);
   });
 
+
   // Test case for missing fields
   it('should return an error for missing fields', async () => {
     const testData: Partial<IFace.IPackageCreated> = {
@@ -333,6 +334,311 @@ describe('validCancel function', () => {
 
     const result = await validCancel(testData);
     expect(result.error).toContain('package already published');
+  });
+});
+
+describe('validPublish function', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // Test case for a valid input
+  it('should return no error for valid input', async () => {
+    // Mock the return value of getPackage function
+    (packageOps.getPackage as jest.Mock).mockResolvedValueOnce({
+      decimals: 8
+    });
+
+    const validInput: IFace.IPackageCreated = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      ticker: '$TC',
+      created: true,
+      initialSupply: '1000',
+      maxSupply: '1000000',
+      decimals: 8,
+      txid: '9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j',
+      data: {
+        digest: "9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j",
+        confirmedLocalExecution: true,
+        transaction: {
+          data: {
+            messageVersion: "v1",
+            sender: "0x8bd6e484961f4d8cf16f5bdd6da78c7ad0d8739933ae36ca0e75a9d17ef79c73"
+          }
+        }
+      },
+    };
+
+    const result = await validPublish(validInput);
+    expect(result.error).toBe('');
+    expect(result.data).toEqual(validInput);
+  });
+
+  // Test case for missing fields
+  it('should return an error for missing fields', async () => {
+    const testData: Partial<IFace.IPackageCreated> = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      ticker: '$TC',
+      txid: '9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j',
+      data: {},
+    };
+
+    const result = await validPublish(testData as IFace.IPackageCreated);
+    expect(result.error).toContain('missing field');
+  });
+
+  // Test case for invalid address
+  it('should return an error for invalid address', async () => {
+    const testData: IFace.IPackageCreated = {
+      address: 'invalid-address',
+      ticker: '$TC',
+      txid: 'txid',
+      data: {},
+      created: true,
+      decimals: 18,
+      initialSupply: '1000',
+      maxSupply: '1000000',
+    };
+
+    const result = await validPublish(testData);
+    expect(result.error).toContain('invalid address');
+  });
+
+  // Test case for incorrect `created` field'
+  it('should return an error for incorrect `created` field', async () => {
+    const testData: IFace.IPackageCreated = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      ticker: '$TC',
+      txid: 'txid',
+      data: {},
+      created: false,
+      decimals: 8,
+      initialSupply: '1000',
+      maxSupply: '1000000',
+    };
+
+    const result = await validPublish(testData);
+    expect(result.error).toContain('created should be true');
+  });
+
+  // Test case for already published package
+  it('should return an error for already published package', async () => {
+    (packageOps.getPackage as jest.Mock).mockResolvedValueOnce({
+      deploy_status: PackageStatus.PUBLISHED,
+    });
+    const testData: IFace.IPackageCreated = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      ticker: '$TC',
+      created: true,
+      initialSupply: '1000',
+      maxSupply: '1000000',
+      decimals: 8,
+      txid: '9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j',
+      data: {
+        digest: "9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j",
+        confirmedLocalExecution: true,
+        transaction: {
+          data: {
+            messageVersion: "v1",
+            sender: "0x8bd6e484961f4d8cf16f5bdd6da78c7ad0d8739933ae36ca0e75a9d17ef79c73"
+          }
+        }
+      },
+    };
+
+    const result = await validPublish(testData);
+    expect(result.error).toContain('package already published: 0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e/tc');
+  });
+
+  // Test case for package not created
+  it('should return an error for package not created', async () => {
+    // Mock getPackage to return null indicating package not created
+    (packageOps.getPackage as jest.Mock).mockResolvedValue(null);
+
+    const testData: IFace.IPackageCreated = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      ticker: '$TC',
+      created: true,
+      initialSupply: '1000',
+      maxSupply: '1000000',
+      decimals: 8,
+      txid: '9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j',
+      data: {
+        digest: "9kdu2wsxGzcf9iKU29y9VSqUDLud9n51SmA72Y6obh2j",
+        confirmedLocalExecution: true,
+        transaction: {
+          data: {
+            messageVersion: "v1",
+            sender: "0x8bd6e484961f4d8cf16f5bdd6da78c7ad0d8739933ae36ca0e75a9d17ef79c73"
+          }
+        }
+      },
+    };
+
+    const result = await validPublish(testData);
+    expect(result.error).toContain('package not created');
+  });
+});
+
+
+describe('validRelatedCreate function', () => {
+  it('should return an error when required fields are missing', async () => {
+    const testData: Partial<IRelationCreate> = {
+      label: '', // Missing label field
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+    };
+
+    const result = await validRelatedCreate(testData as IRelationCreate);
+    expect(result.error).toBeTruthy();
+    expect(result.error).toContain('missing field: label');
+  });
+
+  it('should return an error for invalid address', async () => {
+    const testData: Partial<IRelationCreate> = {
+      label: 'Test Label',
+      address: 'invalidAddress',
+    };
+
+    const result = await validRelatedCreate(testData as IRelationCreate);
+    expect(result.error).toBeTruthy();
+    expect(result.error).toContain('invalid address:');
+  });
+
+  it('should return no error when all fields are valid', async () => {
+    const testData: Partial<IRelationCreate> = {
+      label: 'Test Label',
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+    };
+
+    const result = await validRelatedCreate(testData as IRelationCreate);
+    expect(result.error).toBe('');
+    expect(result.data).toEqual(testData);
+  });
+});
+
+describe('validRelatedDelete function', () => {
+  it('should return an error when label field is missing', async () => {
+    const testData: IFace.IRelationDelete = {
+      label: '', // Missing label field  
+    };
+
+    const result = await validRelatedDelete(testData as IFace.IRelationDelete);
+    expect(result.error).toBeTruthy();
+    expect(result.error).toContain('missing field: label');
+  });
+
+  it('should return no error when label field is provided', async () => {
+    const testData: Partial<IFace.IRelationDelete> = {
+      label: 'Test Label',
+    };
+
+    const result = await validRelatedDelete(testData as IFace.IRelationDelete);
+    expect(result.error).toBe('');
+    expect(result.data).toEqual(testData);
+  });
+});
+
+describe('validRelatedModify function', () => {
+  it('should return an error when label field is missing', async () => {
+    const testData: IFace.IRelationRename = {
+      label: '', // Missing label field  
+    };
+
+    const result = await validRelatedModify(testData as IFace.IRelationRename);
+    expect(result.error).toBeTruthy();
+    expect(result.error).toContain('missing field: label');
+  });
+
+  it('should return no error when label field is provided', async () => {
+    const testData: Partial<IFace.IRelationDelete> = {
+      label: 'Test Label',
+    };
+
+    const result = await validRelatedDelete(testData as IFace.IRelationDelete);
+    expect(result.error).toBe('');
+    expect(result.data).toEqual(testData);
+  });
+});
+
+describe('validIconRequest function', () => {
+  it('should return an error when required fields are missing', () => {
+    const testDataMissingAddress: Partial<IFace.IPackageIcon> = {
+      // Missing address field
+      fileName: 'icon.png',
+    };
+
+    const testDataMissingFileName: Partial<IFace.IPackageIcon> = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      // Missing fileName field
+    };
+
+    const resultMissingAddress = validIconRequest(testDataMissingAddress as IFace.IPackageIcon);
+    expect(resultMissingAddress.error).toBeTruthy();
+    expect(resultMissingAddress.error).toContain('missing field: address');
+
+    const resultMissingFileName = validIconRequest(testDataMissingFileName as IFace.IPackageIcon);
+    expect(resultMissingFileName.error).toBeTruthy();
+    expect(resultMissingFileName.error).toContain('missing field: fileName');
+  });
+
+  it('should return an error when address field is invalid', () => {
+    const testDataInvalidAddress: Partial<IFace.IPackageIcon> = {
+      address: 'invalid_address',
+      fileName: 'icon.png',
+    };
+
+    const resultInvalidAddress = validIconRequest(testDataInvalidAddress as IFace.IPackageIcon);
+    expect(resultInvalidAddress.error).toBeTruthy();
+    expect(resultInvalidAddress.error).toContain('invalid address');
+  });
+
+  it('should return no error when all fields are valid', () => {
+    const testDataValid: Partial<IFace.IPackageIcon> = {
+      address: '0x7b176b89ab5ed899d17b05ffb67b39eeda8aca3e7f41e40353937ed8c943725e',
+      fileName: 'icon.png',
+    };
+
+    const result = validIconRequest(testDataValid as IFace.IPackageIcon);
+    expect(result.error).toBeFalsy();
+    expect(result.data).toEqual(testDataValid);
+  });
+});
+
+describe('isValidTicker function', () => {
+
+  it('should return an error for empty ticker', () => {
+    const result = isValidTicker('');
+    expect(result).toContain('invalid ticker name');
+  });
+
+  it('should return an error for ticker with just $', () => {
+    const result = isValidTicker('$');
+    expect(result).toContain('invalid ticker name');
+  });
+
+  it('should return an error for ticker longer than 6 characters', () => {
+    const result = isValidTicker('$TOOLONGTICKER');
+    expect(result).toContain('invalid ticker name');
+  });
+
+  it('should return an error for ticker not starting with $', () => {
+    const result = isValidTicker('BTC');
+    expect(result).toContain('ticker must start with $');
+  });
+
+  it('should return an error for ticker with special characters', () => {
+    const result = isValidTicker('$BTC@');
+    expect(result).toContain('ticker must be alphanumeric');
+  });
+
+  it('should return an error for reserved ticker names', () => {
+    const result = isValidTicker('$COIN');
+    expect(result).toContain('ticker name cannot be');
+  });
+
+  it('should return no error for valid ticker', () => {
+    const result = isValidTicker('$BTC');
+    expect(result).toBe('');
   });
 });
 
