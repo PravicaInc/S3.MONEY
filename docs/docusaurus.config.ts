f669f8aa894cd7f060ef8adce59aff46ff0407ee
Imamah-Zafar <imamah.zafar@tintash.com>
2024-04-23 21:02:19 +0500

    Remove version badge

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import type * as Preset from '@docusaurus/preset-classic';
 import type { Config } from '@docusaurus/types';
 import { themes as prismThemes } from 'prism-react-renderer';
+import versions from './versions.json';
 
 const config: Config = {
   title: 'StableCoin ',
@@ -46,7 +47,13 @@ const config: Config = {
           href: 'https://github.com/PravicaInc/S3.MONEY/issues',
           position: 'right',
         },
+        {
+          type: 'docsVersion',
+          position: 'right',
+          label: `v${versions[0]}`,
+        },
       ],
+
     },
 
     prism: {
---
+++ docs/src/theme/DocVersionBadge/index.tsx
@@ -0,0 +1,8 @@
+import type { Props } from "@theme/DocVersionBadge";
+
+export default function DocVersionBadge({
+  className,
+}: Props): JSX.Element | null {
+  //Remove showing the version badge on every page
+  return null;
+}
